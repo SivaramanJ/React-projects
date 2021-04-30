f886eabe3a0128517ddefb8c002048ffa8865636
SivaramanJ <j.sivaaraman.j@gmail.com>
2021-04-30 13:02:51 +0530

    test 1

--- src/Plans.js
+++ src/Plans.js
@@ -8,7 +8,6 @@ const Plans = () => {
         <div className="plans">
           <Header className="headerClass" />
           <PlanContainer />
-          
           <div className="plansBg"></div>
         </div>
       );
